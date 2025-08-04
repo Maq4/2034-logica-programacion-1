8b376d9db10a4370d749e8066554724940281fe7
Maq4 <juanacorreoparaclases@gmail.com>
2025-08-04 10:29:16 -0400

    Update app.js linea de mostrar intentos

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ while (numeroUsuario != numeroSecreto) {
   numeroUsuario = Number(prompt("Me indicas un número entre 1 y 10 por favor:"));
   console.log(numeroUsuario);
   if (numeroUsuario == numeroSecreto) {
-    alert(`Acertaste, el número es: ${numeroUsuario}`);
+    alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en :${intentos} veces`);
   } else {
     if (numeroUsuario > numeroSecreto) {
       alert('El número secreto es menor');
@@ -27,6 +27,9 @@ if (numeroUsuario == numeroSecreto){
     }
     //alert('Lo siento, no acertaste el numero');
     }
+intentos= intentos+1 ;
+// incremento de contador si no acierta
  }
+
 
 
