09a5eeaebf8b29bebcd65206bd021099c46eb106
Maq4 <juanacorreoparaclases@gmail.com>
2025-08-04 12:06:49 -0400

    Update app.js funcion agregar maximo intentos

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 let numeroSecreto = 4;
 let numeroUsuario = 0;
 let intentos= 1 ;
-let palabraVeces= ´vez´;
+let palabraVeces= 'vez';
 while (numeroUsuario != numeroSecreto) {
   numeroUsuario = Number(prompt("Me indicas un número entre 1 y 10 por favor:"));
   console.log(numeroUsuario);
@@ -30,9 +30,14 @@ if (numeroUsuario == numeroSecreto){
     }
 intentos= intentos+1 ;
 palabraVeces= ´veces´;
+if (intentos> 3){
+  alert('llegaste al número máximo de intentos');
+  break;
+}
 
 // incremento de contador si no acierta
  }
+
 
 
 
