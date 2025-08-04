ef56b6c432bf3ac59ead48279d4cd283d7d752ea
Maq4 <juanacorreoparaclases@gmail.com>
2025-08-04 09:39:09 -0400

    Update app.js corrección de codigo con chat gpt

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
+
+
 let numeroSecreto = 4;
 let numeroUsuario = 0;
 while (numeroUsuario != numeroSecreto) {
-numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
-
+  numeroUsuario = Number(prompt("Me indicas un número entre 1 y 10 por favor:"));
+  console.log(numeroUsuario);
+  if (numeroUsuario == numeroSecreto) {
+    alert(`Acertaste, el número es: ${numeroUsuario}`);
+  } else {
+    if (numeroUsuario > numeroSecreto) {
+      alert('El número secreto es menor');
+    } else {
+      alert('El número secreto es mayor');
+    }
+  }
+}
 console.log(numeroUsuario);
 if (numeroUsuario == numeroSecreto){
-    alert (Acertaste, el número es: ${numeroUsuario});
+    alert (´Acertaste, el número es: ${numeroUsuario´});
   } else{
-if(numeroUsuario > numeroSecreto) {
-alert('El número secreto es menor');
-} else {
-alert('El número secreto es mayor');
-}
-//alert('Lo siento, no acertaste el numero');
-}
-}
+    if(numeroUsuario > numeroSecreto) {
+       alert('Acertaste,El número secreto es menor');
+    } else {
+        alert('El número secreto es mayor');
+    }
+    //alert('Lo siento, no acertaste el numero');
+    }
+ }
+
