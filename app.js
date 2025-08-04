927f0f49effcc3b2d91def59bbb93ec94e6e68c5
Maq4 <juanacorreoparaclases@gmail.com>
2025-08-04 09:28:28 -0400

    Update app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+let numeroSecreto = 4;
+let numeroUsuario = 0;
+while (numeroUsuario != numeroSecreto) {
+numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+
+console.log(numeroUsuario);
+if (numeroUsuario == numeroSecreto){
+    alert (Acertaste, el número es: ${numeroUsuario});
+  } else{
+if(numeroUsuario > numeroSecreto) {
+alert('El número secreto es menor');
+} else {
+alert('El número secreto es mayor');
+}
+//alert('Lo siento, no acertaste el numero');
+}
+}
