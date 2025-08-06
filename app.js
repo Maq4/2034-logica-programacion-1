b0cd6a7cd51eb95116d854df9a4a9e88035944f6
Maq4 <juanacorreoparaclases@gmail.com>
2025-08-05 20:43:54 -0400

    Update app.js actualizacion variable intentos

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let numeroSecreto = 4;
 let numeroUsuario = 0;
 let intentos= 1 ;
 let palabraVeces= 'vez';
+let maximosintentos=3;
 while (numeroUsuario != numeroSecreto) {
   numeroUsuario = Number(prompt("Me indicas un número entre 1 y 10 por favor:"));
   console.log(numeroUsuario);
@@ -30,13 +31,14 @@ if (numeroUsuario == numeroSecreto){
     }
 intentos= intentos+1 ;
 palabraVeces= ´veces´;
-if (intentos> 3){
-  alert('llegaste al número máximo de intentos');
+if (intentos> maximosintentos){
+  alert(´llegaste al número máximo de intentos ${maximosintentos}´);
   break;
 }
 
 // incremento de contador si no acierta
  }
+
 
 
 
